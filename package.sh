9173f3016198f08006b8d35ff167223a4b90cf03
Gintautas Miliauskas <gintautas@miliauskas.lt>
2011-03-28 19:41:18 +0300

    Added some git greps to package.sh for pre-release glance-testing.

--- package.sh
+++ package.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+git --no-pager grep console.log | grep -v package.sh
+git --no-pager grep TODO | grep -v package.sh
+git --no-pager grep XXX | grep -v package.sh
+
 rm -f crackbook.zip
 zip crackbook.zip manifest.json LICENSE README.md *.html *.js images/*.png
